1620a17cdfa8783dc80e1bc93d21d08090a68106
gastongg13 <gastongg13@gmail.com>
2023-02-09 17:04:55 -0300

    cambiando a front

--- api/src/helpers.js
+++
@@ -1,5 +0,0 @@
-//normaliza strings, en esta caso particular, quita acentos y dieresis
-const normalize = (term) =>
-  term.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-
-module.exports = normalize;
